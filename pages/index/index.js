b0287bee9f2031d8e5cc55f2481b62687f9207a2
jackipan <2466366246@qq.com>
2019-10-18 17:08:37 +0800

    map
    添加地图，跳转，图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,10 +7,12 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isRuleShow: true,
+    textMenu: "隐藏"
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +23,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,7 +45,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -51,25 +53,53 @@ Page({
       hasUserInfo: true
     })
   },
-  tel:function(){
+  // test按钮
+  test: function () {
+    console.log('test');
+    console.log(wx.getSystemInfoSync());
+  },
+  tel: function () {
     wx.makePhoneCall({
       phoneNumber: '15227867593',
-      success:function(){
+      success: function () {
         console.log("拨打电话成功。")
       },
-      fail:function(){
+      fail: function () {
         console.log("拨打失败。")
       }
     })
   },
-  num:function(){
+  num: function () {
     wx.addPhoneContact({
       firstName: '磊',
-      lastName:'张',
-      mobilePhoneNumber:'18888888888',
-      weChatNumber:'',
-      addressStreet:'福永',
+      lastName: '张',
+      mobilePhoneNumber: '18888888888',
+      weChatNumber: '',
+      addressStreet: '福永',
     })
+  },
+  gotoMenu: function () {
+    console.log('gotoMenu')
+    this.setData({
+      isRuleShow: this.data.isRuleShow ? false : true,
+      textMenu: this.data.isRuleShow ? "显示" : "隐藏"
+    })
+  },
+  gotoNext: function () {
+    wx.navigateTo({
+      url: '../map/map',
+    })
+  },
+  // close: function(){
+
+  // },
+  onShareAppMessage: function () {
+    console.log('111')
+    return {
+      title: '高手',
+      desc: '没错我就是高手',
+      path: '/page/user?id=123'
+    }
   }
 
 
